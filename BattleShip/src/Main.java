ad091b47cf63c2801d47283e3c31082744ce6e0e
Ivan <vanya.korobov@yandex.ru>
2023-04-18 17:56:03 +0300

    Третий коммит

--- BattleShip/src/Main.java
+++ BattleShip/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("ПРИВЕТ");
         System.out.println("fevkefmvkef");
         System.out.println("Hello world!");
     }
